608b9a0bb92776ff6993633e8e554b8eb4354011
Sean-Conrad <98624752+Sean-Conrad@users.noreply.github.com>
2023-01-30 16:02:32 -0600

    Add project content

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function Experience(){
     return(
-        <section class="resume-section">
+        <section class="exhibit-section">
             <h1>Experience</h1>
             <div class="row">
               <div class="tier-column col-lg-4">
@@ -12,16 +12,30 @@ function Experience(){
                     <img
                       src="images/PMVCorps.png"
                       alt="PMV-logo"
-                      style={{ width: "100%" }}
+                      style={{ width: "80%" }}
                     />
                   </div>
-                  <div class="card-body">woah
+                  <div class="card-body">
                   </div>
                 </div>
               </div>
-              <div class="col-lg-6">
-                <h3 class="experience-text">Okay</h3>
-                <p>iuhdsf iuhfsdiu uiiufs huisdfhis iuhduhfu uh ushhf uud hdufushdfi hufhiu</p>
+              <div class="col-lg-4">
+                <h3 class="experience-text"></h3>
+                <p>• Redesigned website using technologies (HTML, CSS, JavaScript, Node.js, Bootstrap, Amazon Web Services)</p>
+                <p>• Created a sleek 7-page design to highlight projects and improve the companies’ contract hit rate and hiring process.</p>
+                <p>• Addressed SEO through fully responsive design, keywords, and image naming/compression via Adobe Lightroom</p>
+                <p>• Constituted credibility by including approved, positive testimonials from previous company partners and listing important certifications/credentials</p>
+                <p>• Distinguished between both companies though a designated Installations page and Temps Agency page</p>
+                <p>• Deployed the final application while implementing agile principles and CI/CD (Git and AWS)</p>
+                <p>• Responsible for regular maintenance, updates, and hosting services</p>
+              </div>
+              <div class="col-lg-4">
+                    <img
+                      src="images/PMV.jpeg"
+                      alt="PMV-logo"
+                      style={{ width: "100%" }}
+                    />
+                    <a class="button-link" href="https://pmvcorps.com">Visit!</a>
               </div>
             </div>
         </section>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,15 +2,16 @@ import React from "react";
 
 function Projects() {
   return (
-    <section class="projects-section">
+    <section class="exhibit-section">
       <h1>Projects</h1>
-      <div class="row">
-        <div class="tier-column col-lg-4">
+{/* Dashboard  */} 
+      <div class="row container-1">
+        <div class="tier-column col-lg-3">
           <div class="card" style={{ background: "#030017" }}>
             <div class="card-header">
               <h2>Dashboard</h2>
               <img
-                src="images/PMVCorps.png"
+                src="images/Dashboard.jpeg"
                 alt="PMV-logo"
                 style={{ width: "100%" }}
               />
@@ -19,34 +20,153 @@ function Projects() {
           </div>
         </div>
 
-        <div class="tier-column col-lg-4">
+        <div class="tier-column col-lg-3">
           <div class="card" style={{ background: "#030017" }}>
             <div class="card-header">
-              <h2>Smart Dashcam</h2>
-              <img
-                src="images/PMVCorps.png"
-                alt="PMV-logo"
-                style={{ width: "100%" }}
-              />
+              <p>Nisi ullamco elit duis ipsum proident irure eu ad. Officia commodo qui reprehenderit amet cupidatat nisi sit do quis occaecat esse magna culpa. 
+              Proident excepteur cillum laboris laboris et fugiat incididunt deserunt veniam velit. Adipisicing dolore officia aliqua aliqua velit officia 
+              Lorem aute est aute elit id. Consequat in esse consectetur in adipisicing dolor aliqua non commodo cupidatat proident dolor ex.</p>
+              <a class="button-link" href="https://github.com/Sean-Conrad/Dashboard">View on Github!</a>
+
             </div>
             <div class="card-body"></div>
           </div>
         </div>
 
-        <div class="tier-column col-lg-4">
-          <div class="card" style={{ background: "#030017" }}>
-            <div class="card-header">
-              <h2>Google Keep Clone</h2>
-              <img
-                src="images/PMVCorps.png"
-                alt="PMV-logo"
-                style={{ width: "100%" }}
-              />
-            </div>
-            <div class="card-body"></div>
-          </div>
+        <div class="tier-column col-lg-6">
+          <video width="900" contols>
+              <source src="images/Dashboard-demo.mov" type="video/mp4"/>
+            </video>
         </div>
       </div>
+
+{/* Google Keep Clone */}
+      <div class="row container-1">
+      <div class="tier-column col-lg-3">
+        <div class="card" style={{ background: "#030017" }}>
+          <div class="card-header">
+            <h2>Google Keep Clone</h2>
+            <img
+              src="images/Google-keep-clone.jpeg"
+              alt="PMV-logo"
+              style={{ width: "100%" }}
+            />
+          </div>
+          <div class="card-body"></div>
+        </div>
+      </div>
+
+      <div class="tier-column col-lg-3">
+        <div class="card" style={{ background: "#030017" }}>
+          <div class="card-header">
+            <p>Nisi ullamco elit duis ipsum proident irure eu ad. Officia commodo qui reprehenderit amet cupidatat nisi sit do quis occaecat esse magna culpa. 
+            Proident excepteur cillum laboris laboris et fugiat incididunt deserunt veniam velit. Adipisicing dolore officia aliqua aliqua velit officia 
+            Lorem aute est aute elit id. Consequat in esse consectetur in adipisicing dolor aliqua non commodo cupidatat proident dolor ex.
+            </p>
+            <a class="button-link" href="https://secure-shelf-65021.herokuapp.com/">Visit!</a>
+          </div>
+          <div class="card-body"></div>
+        </div>
+      </div>
+
+      <div class="tier-column col-lg-6">
+          <img
+            src="images/Google-keep-clone.jpeg"
+            alt="PMV-logo"
+            style={{ width: "100%" }}
+          />
+        {/* <video width="600" contols>
+            <source src="images/Dashboard-demo.mov" type="video/mp4"/>
+          </video> */}
+      </div>
+    </div>
+
+{/* Task List */}
+<div class="row container-1">
+      <div class="tier-column col-lg-3">
+        <div class="card" style={{ background: "#030017" }}>
+          <div class="card-header">
+            <h2>Google Keep Clone</h2>
+            <img
+              src="images/Task-list.jpeg"
+              alt="PMV-logo"
+              style={{ width: "100%" }}
+            />
+          </div>
+          <div class="card-body"></div>
+        </div>
+      </div>
+
+      <div class="tier-column col-lg-3">
+        <div class="card" style={{ background: "#030017" }}>
+          <div class="card-header">
+            <p>Nisi ullamco elit duis ipsum proident irure eu ad. Officia commodo qui reprehenderit amet cupidatat nisi sit do quis occaecat esse magna culpa. 
+            Proident excepteur cillum laboris laboris et fugiat incididunt deserunt veniam velit. Adipisicing dolore officia aliqua aliqua velit officia 
+            Lorem aute est aute elit id. Consequat in esse consectetur in adipisicing dolor aliqua non commodo cupidatat proident dolor ex.
+            https://serene-shelf-88879.herokuapp.com/</p>
+            <a class="button-link" href="https://serene-shelf-88879.herokuapp.com/">Visit!</a>
+          </div>
+          <div class="card-body"></div>
+        </div>
+      </div>
+
+      <div class="tier-column col-lg-6">
+          <img
+            src="images/Task-list.jpeg"
+            alt="PMV-logo"
+            style={{ width: "100%" }}
+          />
+        {/* <video width="600" contols>
+            <source src="images/Dashboard-demo.mov" type="video/mp4"/>
+          </video> */}
+      </div>
+    </div>
+
+{/* Dashcam */}
+<div class="row container-1">
+      <div class="tier-column col-lg-3">
+        <div class="card" style={{ background: "#030017" }}>
+          <div class="card-header">
+            <h2>Dashcam</h2>
+            <img
+              src="images/Dashcam.jpeg"
+              alt="PMV-logo"
+              style={{ width: "100%" }}
+            />
+          </div>
+          <div class="card-body"></div>
+        </div>
+      </div>
+
+      <div class="tier-column col-lg-3">
+        <div class="card" style={{ background: "#030017" }}>
+          <div class="card-header">
+            <p>Nisi ullamco elit duis ipsum proident irure eu ad. Officia commodo qui reprehenderit amet cupidatat nisi sit do quis occaecat esse magna culpa. 
+            Proident excepteur cillum laboris laboris et fugiat incididunt deserunt veniam velit. Adipisicing dolore officia aliqua aliqua velit officia 
+            Lorem aute est aute elit id. Consequat in esse consectetur in adipisicing dolor aliqua non commodo cupidatat proident dolor ex.
+            </p>
+            <a class="button-link" href="https://github.com/Sean-Conrad/Smart-Dash-Cam">View on Github!</a>
+          </div>
+          <div class="card-body"></div>
+        </div>
+      </div>
+
+      <div class="tier-column col-lg-6">
+          {/* <img
+            src="images/Dashcam.jpeg"
+            alt="PMV-logo"
+            style={{ width: "100%" }}
+          /> */}
+        {/* <video width="600" contols>
+            <source src="images/Dashboard-demo.mov" type="video/mp4"/>
+          </video> */}
+          <iframe title="FOX WIRE EXPO" 
+            src="https://player.vimeo.com/video/532449896?h=eab9e45d7d&amp;dnt=1&amp;app_id=122963" 
+            width="900" height="480" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" 
+            allowfullscreen="">
+          </iframe>
+      </div>
+    </div>
     </section>
   );
 }
